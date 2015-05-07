21f27433e0068748298762eee39130a932c68f80
hexiaojiao <xiaojiaopower@163.com>
2015-05-07 14:55:32 +0800

    hxj 2015/5/7

--- ctg-itrdc-account-balance-service2/src/main/java/com/ctg/itrdc/account/balance/service/impl/TestCommitServiceImpl.java
+++ ctg-itrdc-account-balance-service2/src/main/java/com/ctg/itrdc/account/balance/service/impl/TestCommitServiceImpl.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ctg.itrdc.account.balance.dao.RouteDao;
 import com.ctg.itrdc.account.balance.model.AcctBalanceModel;
 import com.ctg.itrdc.account.balance.repository.IAcctBalanceMapper;
 import com.ctg.itrdc.account.balance.repository.ITestMapper;
