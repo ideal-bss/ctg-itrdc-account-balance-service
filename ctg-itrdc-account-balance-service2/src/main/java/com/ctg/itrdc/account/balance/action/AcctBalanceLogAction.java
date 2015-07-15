5a5551509e35ac526bc16ed1e350046aad5e2cc8
liusai163 <wuziqingteng@163.com>
2015-07-15 14:56:13 +0800

    ls

--- ctg-itrdc-account-balance-service2/src/main/java/com/ctg/itrdc/account/balance/action/AcctBalanceLogAction.java
+++ ctg-itrdc-account-balance-service2/src/main/java/com/ctg/itrdc/account/balance/action/AcctBalanceLogAction.java
@@ -1,7 +1,6 @@
 package com.ctg.itrdc.account.balance.action;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
@@ -21,7 +20,12 @@ public class AcctBalanceLogAction extends BaseAction {
 	Logger logger = Logger.getLogger(AcctBalanceLogAction.class);
 	private long acctId;
 	private long balanceTypeId;
+	private long acctBalanceId;
+	private long operIncomeId;
 	private IAcctBalanceService iAcctBalanceService;
+	
+	private int rows;
+	private int page;
 	/**
 	 * 
 	 * @desc 界面跳转
@@ -41,15 +45,17 @@ public class AcctBalanceLogAction extends BaseAction {
 	 */
 	public String acctBalanceLog(){
 		logger.debug("acctBalanceLog()......start......");
-		List<Object> resultList = null;
+		Map<String, Object> mapList = null;
 		try {
 			
 			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("acctId", acctId);
 			map.put("balanceTypeId", balanceTypeId);
-			resultList = iAcctBalanceService.queryAcctBalanceLog(map);
+			map.put("rows", rows);
+			map.put("page", ((page-1)*rows));
+			mapList = iAcctBalanceService.queryAcctBalanceLog(map);
 			
-			writeJson(resultList);
+			writeJson(mapList);
 		} catch (Exception e) {
 			logger.debug(e.getMessage());
 			e.printStackTrace();
@@ -57,6 +63,55 @@ public class AcctBalanceLogAction extends BaseAction {
 			logger.debug("acctBalanceLog()......end......");
 		}
 		
+		return "success";
+	}
+	
+	/**
+	 * 
+	 * @desc 余额来源查询
+	 * @author ls
+	 * @return
+	 */
+	public String acctBalLogbalSourceQuery(){
+		logger.debug("acctBalLogbalSourceQuery()......start......");
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("sliceKey", acctId);
+			map.put("acctBalanceId", acctBalanceId);
+			map.put("rows", rows);
+			map.put("page", ((page-1)*rows));
+			Map<String, Object> result = iAcctBalanceService.acctBalLogbalSourceQuery(map);
+			writeJson(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			logger.debug("acctBalLogbalSourceQuery()......end......");
+		}
+		return "success";
+	}
+	
+	/**
+	 * 
+	 * @desc 余额支出记录查询
+	 * @author ls
+	 * @return
+	 */
+	public String acctBalLogbalPayOutQuery(){
+		logger.debug("acctBalLogbalSourceQuery()......start......");
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("sliceKey", acctId);
+			map.put("acctBalanceId", acctBalanceId);
+			map.put("operIncomeId", operIncomeId);
+			map.put("rows", rows);
+			map.put("page", ((page-1)*rows));
+			Map<String, Object> result = iAcctBalanceService.acctBalLogbalPayOutQuery(map);
+			writeJson(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			logger.debug("acctBalLogbalSourceQuery()......end......");
+		}
 		return "success";
 	}
 
@@ -83,6 +138,38 @@ public class AcctBalanceLogAction extends BaseAction {
 	@Autowired
 	public void setiAcctBalanceService(IAcctBalanceService iAcctBalanceService) {
 		this.iAcctBalanceService = iAcctBalanceService;
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getPage() {
+		return page;
+	}
+
+	public void setPage(int page) {
+		this.page = page;
+	}
+
+	public long getAcctBalanceId() {
+		return acctBalanceId;
+	}
+
+	public void setAcctBalanceId(long acctBalanceId) {
+		this.acctBalanceId = acctBalanceId;
+	}
+
+	public long getOperIncomeId() {
+		return operIncomeId;
+	}
+
+	public void setOperIncomeId(long operIncomeId) {
+		this.operIncomeId = operIncomeId;
 	}
 	
 	
